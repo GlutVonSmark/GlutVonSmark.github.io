c08bef83c96bb04207fc8fa01d86f980a957727f
Juszczyk Piotr (YWX7RWK) <YWX7RWK@UPS.COM>
2019-05-08 14:40:40 +0100

    Initial commit

---
+++ precache-manifest.6eeb4b0585bec56b331f0987564f62d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4c8a0f2248983ef3e704c74cceb42e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f56c0ebc42caa91f7b2",
+    "url": "/static/css/main.c57c989e.chunk.css"
+  },
+  {
+    "revision": "cae37e09ff8abb16f63c",
+    "url": "/static/js/2.2ba7af8c.chunk.js"
+  },
+  {
+    "revision": "2f56c0ebc42caa91f7b2",
+    "url": "/static/js/main.131df49d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22c7df073833fef0e1508a36cf0d0f9f",
+    "url": "/static/media/DIOGENES.22c7df07.ttf"
+  },
+  {
+    "revision": "fe0e5662e65288c0e880bbd2c5e81b1d",
+    "url": "/static/media/pythia.fe0e5662.ttf"
+  }
+]);
